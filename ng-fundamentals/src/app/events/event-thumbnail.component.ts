10fdb8fbbae4be0d2882efb4d8cf1d39b2950add
José Silva <jcnascimento1994@gmail.com>
2020-01-14 11:05:03 -0300

    Using ngIf and "hidden"

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -4,16 +4,17 @@ import { Component, Input } from "@angular/core";
   selector: "event-thumbnail",
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{ event.name }}</h2>
-      <div>Date: {{ event.date }}</div>
-      <div>Time: {{ event.time }}</div>
-      <div>Price: \${{ event.price }}</div>
-      <div>
-        <span>Location: {{ event.location.address }}</span>
+      <h2>{{ event?.name }}</h2>
+      <div>Date: {{ event?.date }}</div>
+      <div>Time: {{ event?.time }}</div>
+      <div>Price: \${{ event?.price }}</div>
+      <div [hidden]="!event?.location">
+        <span>Location: {{ event?.location.address }}</span>
         <span class="pad-left"
-          >{{ event.location.city }}, {{ event.location.country }}</span
+          >{{ event?.location.city }}, {{ event?.location.country }}</span
         >
       </div>
+      <div *ngIf="event?.onlineUrl">Online URL: {{ event?.onlineUrl }}</div>
     </div>
   `,
   styles: [
