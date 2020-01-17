db9a79b640b74df3012981ec1f77ae3d80161bce
José Silva <jcnascimento1994@gmail.com>
2020-01-17 08:16:11 -0300

    Using reactive forms

--- ng-fundamentals/src/app/user/user.module.ts
+++ ng-fundamentals/src/app/user/user.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { ProfileComponent } from "./profile.component";
 import { userRoutes } from "./user.routes";
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(userRoutes), FormsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(userRoutes),
+    FormsModule,
+    ReactiveFormsModule
+  ],
   declarations: [ProfileComponent],
   providers: []
 })
